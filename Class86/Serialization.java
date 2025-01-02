435c23ffd0534c076bd6ccc94c66212aabba298d
Abhisek Nayak <cv07abhisek@gmail.com>
2025-01-02 23:27:56 +0530

    Understading Serialization

---
+++ Class86/Serialization.java
@@ -0,0 +1,20 @@
+package Class86;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Serialization {
+    public static void main(String[] args) throws IOException {
+        Student student = new Student(101, "Abhi");
+        System.out.println(student);
+        System.out.println("THIS OBJECT WILL BE REMOVED IN 2MS AFTER PROGRAM EXECUTES.");
+
+        String path = "E:\\Kodenest\\JAVA\\Class86\\student.txt";
+        FileOutputStream fos = new FileOutputStream(path);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(student
+        );
+
+    }
+}
